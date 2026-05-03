8aab40ba3117deaeb4c79c764e479a79d9ac3d8b
Arnav <arnav.vyas06@gmail.com>
2026-05-03 11:00:00 +0530

    feat(repo): add UserRepo with findByClerkId query

---
+++ src/main/java/com/arnav/tutionSAAS/repository/UserRepo.java
@@ -0,0 +1,12 @@
+package com.arnav.tutionSAAS.repository;
+
+import com.arnav.tutionSAAS.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    Optional<User> findByClerkId(String clerkId);
+}
